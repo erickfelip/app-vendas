035bc33321842694912861674e4a565d0c9e49d0
Erick Felipe <erickfelipe2010@gmail.com>
2022-01-10 16:21:27 -0300

    feat: Create custom tab navigation

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -1,22 +1,63 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+import { MaterialIcons, MaterialCommunityIcons  } from '@expo/vector-icons'
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
 import { Dashboard } from '../screens/Dashboard';
+import { useTheme } from 'styled-components';
 
 export function AppRoutes(){
+    const theme = useTheme();
+
     return(
-        <Navigator>
+        <Navigator
+        screenOptions={{
+            headerShown: false,
+            tabBarActiveBackgroundColor: theme.colors.primary,
+            tabBarLabelPosition: 'beside-icon',
+            tabBarStyle: {
+                height: 50,
+            }
+        }}
+        >
             <Screen 
             name='Início'
-            component={Dashboard}/>
+            component={Dashboard}
+            options={{
+                tabBarIcon: (({ size, color}) => 
+                <MaterialIcons 
+                name="format-list-bulleted"
+                size={size}
+                color={color}
+                />
+                )
+            }}
+            />
             <Screen 
             name='Saldo'
-            component={Dashboard}/>
+            component={Dashboard}
+            options={{
+                tabBarIcon: (({ size, color}) => 
+                <MaterialIcons 
+                name="attach-money"
+                size={size}
+                color={color}
+                />
+                )
+            }}/>
             <Screen 
             name='Vendas'
-            component={Dashboard}/>
+            component={Dashboard}
+            options={{
+                tabBarIcon: (({ size, color}) => 
+                <MaterialCommunityIcons 
+                name="cart-outline"
+                size={size}
+                color={color}
+                />
+                )
+            }}/>
         </Navigator>
     )
 }
